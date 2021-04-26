d652be8f32ed64cc2393188e94a1be789496fa88
kunatastic <kjha6666@gmail.com>
2021-04-26 12:41:00 +0530

    added dynamic AMAZON URI

--- routes/AWSRoutes.js
+++ routes/AWSRoutes.js
@@ -4,9 +4,7 @@ const fetch = require("node-fetch");
 
 Router.get("/raw", async (req, res) => {
   try {
-    const data = await fetch(
-      "https://o0uz9k85x4.execute-api.us-east-1.amazonaws.com/sentiment-final/"
-    );
+    const data = await fetch(process.env.AMAZON_URI);
     const values = await data.json();
     res.json(values);
   } catch (e) {
