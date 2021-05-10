437e311bb19673165955cee3221ecb39bd0b3be0
kunatastic <kjha6666@gmail.com>
2021-05-11 02:50:26 +0530

    algo done

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { main } = require("../Algo");
 const Router = express.Router();
 const { checkAuthenticated } = require("../middlewares/AuthMiddleWare");
 const { Face } = require("../models/FaceDataModel");
+const { Text } = require("../models/TextDataModel");
 
 Router.get("/", checkAuthenticated, (req, res) => {
   // console.log(req);
@@ -9,8 +11,15 @@ Router.get("/", checkAuthenticated, (req, res) => {
 });
 
 Router.get("/analysis", checkAuthenticated, async (req, res) => {
-  const allSentiments = await Face.find({ user_id: req.user._id });
-  res.render("analysis.ejs", { sentiments: allSentiments });
+  res.render("analysis.ejs");
+});
+Router.get("/analysis/raw", checkAuthenticated, async (req, res) => {
+  const FaceSentiments = await Face.find({ user_id: req.user._id });
+  const TextSentiments = await Text.find({ user_id: req.user._id });
+  const data = main(FaceSentiments, TextSentiments);
+  // const allSentiments = { FaceSentiments, TextSentiments };
+  console.log(data);
+  // res.json(allSentiments);
 });
 
 module.exports = Router;
